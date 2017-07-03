f0be03d294f8c47d69ac78e5da907ab88c90acb0
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-07-03 14:34:48 -0500

    final

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 var friends=['Harry','Larry','Barry','Scary','Dairy']
 var loc=['Dining Room','Living Room','Dying Room','Existing Room','Blood Room','Stab Room','Stabby Death Room of Doom','Utility Closet','Death Closet','Stab Closet']
 var weapons=['Knife','Loaf of Bread','Dog Biscuit','Bag of Flour','Keyboard','JavaScript Lab','Fireworks','Flail','Mace','Javelin','Sword','Axe','Vacuum Cleaner','Cat','Headphones','Giraffe','Zebra Skull','Playstation','Water Bottle','Mustard']
-var randFriend = friends[Math.floor(Math.random() * friends.length)];
-var randLoc = loc[Math.floor(Math.random() * loc.length)];
-var randWep = weapons[Math.floor(Math.random() * weapons.length)];
+
 
 $(document).ready(function(){
 
@@ -13,14 +11,17 @@ $(document).ready(function(){
         document.body.append(header)
         var clue="Clue " + i
         header.append(clue)
-        
+    
     }
-    $('.heading').on('click', function(){
+})
+
+    $(this).bind('click', function(){
         clueGame()
 })
 
-})
-
 function clueGame(){
+    var randFriend = friends[Math.floor(Math.random() * friends.length)];
+    var randLoc = loc[Math.floor(Math.random() * loc.length)];
+    var randWep = weapons[Math.floor(Math.random() * weapons.length)];
     alert(randFriend + ' did it in ' + randLoc + ' with a ' + randWep)
 }
